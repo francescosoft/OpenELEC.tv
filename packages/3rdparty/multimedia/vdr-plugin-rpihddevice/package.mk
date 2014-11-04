50e120a9e45614e155b374355a55e57fcd1124bb
Franz Osvald <fo@cylex.ro>
2014-11-05 00:07:58 +0200

    updated vdr-rpihddevice

--- packages/3rdparty/multimedia/vdr-plugin-rpihddevice/package.mk
+++ packages/3rdparty/multimedia/vdr-plugin-rpihddevice/package.mk
@@ -19,7 +19,7 @@
     ################################################################################
      
     PKG_NAME="vdr-plugin-rpihddevice"
-    PKG_VERSION="809daf6"
+    PKG_VERSION="b3867e4"
     PKG_REV="1"
     PKG_ARCH="any"
     PKG_LICENSE="GPL"
